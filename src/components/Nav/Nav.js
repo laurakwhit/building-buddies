72cf7bb8f716455fec99cca86c2dc52c31ca26fc
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-15 20:55:35 -0600

    Style Nav and change home route back to LandingPage

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,22 +6,30 @@ import './Nav.scss';
 
 const Nav = () => {
   return (
-    <div className='Nav_Section'>
-      <img src='../assets/profile.jpg' alt='profile picture'/>
+    <div className='nav__section'>
+      <img src={require('../../assets/profile.jpg')} alt='profile picture'/>
       <nav>
         <NavLink
+          className='nav__section-links'
+          activeClassName='selected'
           to='/profile'>
           My Profile
         </NavLink>
         <NavLink
+          className='nav__section-links'
+          activeClassName='selected'
           to='/neighbors'>
           My Neighbors
         </NavLink>
         <NavLink
+          className='nav__section-links'
+          activeClassName='selected'
           to='/building'>
           Building Info
         </NavLink>
         <NavLink
+          className='nav__section-links'
+          activeClassName='selected'
           to='/'>
           Log Out
         </NavLink>
