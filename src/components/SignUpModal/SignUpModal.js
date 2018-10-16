8e690c2401255b5f534e57422d8642b432317fe4
Gray Smith <gray.smith00@gmail.com>
2018-10-15 22:46:09 -0600

    Build out SignUpModal form, integrate complete-me for building search results

--- src/components/SignUpModal/SignUpModal.js
+++ src/components/SignUpModal/SignUpModal.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { PrefixTrie } from 'complete-me';
 import './SignUpModal.scss';
 
 class SignUpModal extends Component {
   state = {
+    searchValue: '',
+    autoCompleteResults: [],
     name: '',
-    email: ''
+    email: '',
+    buildingsTrie: {}
+  };
+
+  componentDidMount() {
+    const { buildings } = this.props;
+    const buildingsTrie = new PrefixTrie();
+    const buildingNames = buildings.map(b => b.name);
+    buildingsTrie.populate(buildingNames);
+    this.setState({ buildingsTrie });
+  }
+
+  handleSearchChange = e => {
+    const { buildingsTrie } = this.state;
+    const autoCompleteResults = buildingsTrie.suggest(e.target.value);
+    this.setState({ autoCompleteResults, searchValue: e.target.value });
   };
 
   handleChange = e => {
@@ -16,12 +34,26 @@ class SignUpModal extends Component {
   handleSubmit = e => {
     e.preventDefault();
     this.props.setUser(this.state);
-    this.setState({ name: '', email: '' });
+    this.setState({ name: '', email: '', searchValue: '' });
+  };
+
+  handleSuggestionClick = e => {
+    this.setState({ searchValue: e.target.value });
   };
 
   render() {
     const { handleModalClose } = this.props;
-    const { name, email } = this.state;
+    const { name, email, autoCompleteResults, searchValue } = this.state;
+
+    const suggestionOptions = autoCompleteResults.map((suggestion, i) => (
+      <option
+        onClick={this.handleSuggestionClick}
+        key={`${suggestion}-${i}`}
+        value={suggestion}
+      >
+        {suggestion}
+      </option>
+    ));
 
     return (
       <div className="sign-up-modal">
@@ -30,16 +62,25 @@ class SignUpModal extends Component {
           <p onClick={handleModalClose}>X</p>
           <form onSubmit={this.handleSubmit} className="sign-up-form">
             <input
+              onChange={this.handleSearchChange}
+              type="text"
+              placeholder="My Building"
+              value={searchValue}
+            />
+            <datalist id="suggestions">{suggestionOptions}</datalist>
+            <input
               onChange={this.handleChange}
               type="name"
               name="name"
               value={name}
+              placeholder="Name"
             />
             <input
               onChange={this.handleChange}
               type="email"
               name="email"
               value={email}
+              placeholder="Email"
             />
             <button>Sign Up</button>
           </form>
