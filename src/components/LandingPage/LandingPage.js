3a6d3443bcb9a919a9a4492c8d68fea95e4f8ec0
Gray Smith <gray.smith00@gmail.com>
2018-10-15 21:20:16 -0600

    Build out LandingPage component, handle modalOpen state, render SignUpModal

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import './LandingPage.scss';
 
+import SignUpModal from '../SignUpModal/SignUpModal';
+
 class LandingPage extends Component {
+  state = {
+    modalOpen: false
+  };
+
+  handleClick = () => {
+    this.setState({ modalOpen: true });
+  };
+
+  handleModalClose = () => {
+    this.setState({ modalOpen: false });
+  };
+
   render() {
+    const { modalOpen } = this.state;
+    const { setUser } = this.props;
+
     return (
-      <h1>Hello!</h1>
+      <div className="landing-page">
+        {modalOpen && (
+          <SignUpModal
+            handleModalClose={this.handleModalClose}
+            setUser={setUser}
+          />
+        )}
+        <h1>Hello!</h1>
+        <button onClick={this.handleClick}>Sign Up</button>
+      </div>
     );
   }
 }
