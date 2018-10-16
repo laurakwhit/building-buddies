4d16bd595494246370b5448227a8f985ae2d2c9c
Gray Smith <gray.smith00@gmail.com>
2018-10-15 18:02:38 -0600

    Move state to Routes component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,8 @@
 import React, { Component } from 'react';
-import { getAllBuildings } from '../../utilities/apiCalls';
 import Routes from '../Routes/Routes';
 import './App.scss';
 
 class App extends Component {
-  state = {
-    buildings: [],
-    interests: [],
-    currentUser: {},
-    userBuilding: {},
-    userInterests: []
-  };
-
-  async componentDidMount() {
-    const buildings = await getAllBuildings();
-    console.log(buildings);
-  }
-
   render() {
     return (
       <div className="App">
--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { getAllBuildings } from '../../utilities/apiCalls';
 import LandingPage from '../LandingPage/LandingPage';
 
 class Routes extends Component {
+  state = {
+    buildings: [],
+    interests: [],
+    currentUser: {},
+    userBuilding: {},
+    userInterests: []
+  };
+
+  async componentDidMount() {
+    const buildings = await getAllBuildings();
+    console.log(buildings);
+  }
+
   render() {
     return (
       <Router>
