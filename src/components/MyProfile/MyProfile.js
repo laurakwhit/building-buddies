15993122245cc7ae9758a77a9c8e9673e013f7dc
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-17 12:08:27 -0600

    Change method to handleUserInterestChange

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { getUserInterests } from '../../utilities/userApiCalls';
-import { addUserInterest, deleteUserInterest } from '../../utilities/userApiCalls';
+import {
+  addUserInterest,
+  deleteUserInterest
+} from '../../utilities/userApiCalls';
 
 import Nav from '../Nav/Nav';
 
@@ -18,59 +21,67 @@ class MyProfile extends Component {
     this.setState({ userInterests });
   }
 
-  updateUserInterests = (e) => {
+  handleUserInterestChange = e => {
     const { userInterests } = this.state;
-    const { currentUser } = this.props;
-    const interest = JSON.parse(e.target.value);
-    
-    userInterests.forEach(async i => {
-      let updatedInterests = [];
-      if (i.name === interest.name) {
-        updatedInterests = userInterests.filter(i => i.name !== interest.name);
-        // await deleteUserInterest(currentUser.id, i.id)
-      } else {
-        updatedInterests = [...userInterests, interest];
-        // await addUserInterest(currentUser.id, i.id);
-      }
-      this.setState({ userInterests: updatedInterests })
-    })
+    const { currentUser, interests } = this.props;
+    console.log(userInterests);
+    const alreadyInterested = userInterests.find(
+      interest => interest.id === e.target.value
+    );
 
-  }
+    let newUserInterests;
+
+    if (alreadyInterested) {
+      newUserInterests = userInterests.filter(
+        interest => interest.id !== e.target.value
+      );
+      // await deleteUserInterest(currentUser.id, i.id)
+    } else {
+      const newInterest = interests.find(
+        interest => interest.id === e.target.value
+      );
+      newUserInterests = [...userInterests, newInterest];
+      // await addUserInterest(currentUser.id, i.id);
+    }
+    this.setState({ userInterests: newUserInterests });
+  };
 
   render() {
     const { currentUser, interests, handleLogOut } = this.props;
     const { userInterests } = this.state;
     const displayedInterests = interests.map((interest, index) => {
-      const checked = userInterests.find(i => i.name === interest.name);
+      const checked = userInterests.find(i => i.id === interest.id);
 
-      return <div key={index}>
-        <input 
-          type="checkbox" 
-          name={interest.name} 
-          value={JSON.stringify(interest)} 
-          checked={checked} 
-          onChange={this.updateUserInterests}/> 
-        <label htmlFor={interest.name}>{interest.name}</label>
-      </div>;
+      return (
+        <div key={index}>
+          <input
+            type="checkbox"
+            name={interest.name}
+            value={interest.id}
+            checked={checked}
+            onChange={this.handleUserInterestChange}
+          />
+          <label htmlFor={interest.name}>{interest.name}</label>
+        </div>
+      );
     });
 
     return (
       <div className="my-profile">
         <Nav handleLogOut={handleLogOut} />
         <div className="main-content">
-          <img src={require('../../assets/profile.jpg')} alt='profile' />
+          <img src={require('../../assets/profile.jpg')} alt="profile" />
           <h2>{currentUser.name}</h2>
           <article>
-            Humblebrag shabby chic gochujang coloring book, squid XOXO brunch tumeric
-            selvage hammock vape. Lomo lumbersexual glossier, wayfarers skateboard irony
-            humblebrag raclette taxidermy ennui freegan af craft beer PBR&B meh. Asymmetrical
-            thundercats intelligentsia, lo-fi swag fingerstache palo santo skateboard
-            flexitarian lyft selvage vice poke gluten-free. Kombucha ramps chambray street
-            art messenger bag.
+            Humblebrag shabby chic gochujang coloring book, squid XOXO brunch
+            tumeric selvage hammock vape. Lomo lumbersexual glossier, wayfarers
+            skateboard irony humblebrag raclette taxidermy ennui freegan af
+            craft beer PBR&B meh. Asymmetrical thundercats intelligentsia, lo-fi
+            swag fingerstache palo santo skateboard flexitarian lyft selvage
+            vice poke gluten-free. Kombucha ramps chambray street art messenger
+            bag.
           </article>
-          <div>
-            {displayedInterests}
-          </div>
+          <form>{displayedInterests}</form>
         </div>
       </div>
     );
