3e61308b0f8bb6e6ea706154647ae291c86f59a6
Gray Smith <gray.smith00@gmail.com>
2018-10-16 22:38:06 -0600

    Create LoadingSpinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import spinner from './spinner.gif';
+
+const LoadingSpinner = props => {
+  return (
+    <div>
+      <img src="./spinner.gif" alt="Loading..." />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
