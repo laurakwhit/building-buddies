eb30996eef1f27ad05e74c980fc12405e916e74f
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-10-17 18:40:40 -0600

    Style Neightbors list and profile

--- src/components/NeighborProfile/NeighborProfile.js
+++ src/components/NeighborProfile/NeighborProfile.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Nav from '../Nav/Nav';
-
+import { getUserInterests } from '../../utilities/userApiCalls';
 import './NeighborProfile.scss';
 
 class NeighborProfile extends Component {
+  state = {
+    neighborInterests: []
+  };
+
+  async componentDidMount() {
+    const { neighbor } = this.props;
+    const neighborInterests = await getUserInterests(neighbor.id);
+    this.setState({ neighborInterests });
+  }
+
   render() {
     const { neighbor } = this.props;
+    const { neighborInterests } = this.state;
+    let displayNeighborInterests;
+
+    if (!neighborInterests.length) {
+      displayNeighborInterests = (
+        <p>This user does not have any interests yet!</p>
+      );
+    } else {
+      displayNeighborInterests = neighborInterests.map(
+        (neighborInterest, i) => (
+          <p key={`${neighborInterest.id}-${i}`}>{neighborInterest.name}</p>
+        )
+      );
+    }
+
     return (
       <div className="neighbor-profile">
         <Nav />
         <div className="main-content">
-          <h1>{neighbor.name}</h1>
+          <img
+            src={require('../../assets/profile.jpg')}
+            alt="profile"
+            className="profile-image"
+          />
+          <h1>{neighbor.name} likes...</h1>
+          <div className="interests-container">{displayNeighborInterests}</div>
         </div>
       </div>
     );
