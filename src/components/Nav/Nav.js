42195e45301baf8cfee44a3b0260104e53bdeb9e
Gray Smith <gray.smith00@gmail.com>
2018-10-17 20:06:42 -0600

    Add Account NavLink to Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,35 +6,38 @@ import { NavLink } from 'react-router-dom';
 import './Nav.scss';
 
 const Nav = ({ handleLogOut }) => (
-    <div className='nav__section'>
-      <img src={require('../../assets/profile.jpg')} alt='profile'/>
-      <nav>
-        <NavLink
-          className='nav__section-links'
-          activeClassName='selected'
-          to='/profile'>
-          My Profile
-        </NavLink>
-        <NavLink
-          className='nav__section-links'
-          activeClassName='selected'
-          to='/neighbors'>
-          My Neighbors
-        </NavLink>
-        <NavLink
-          className='nav__section-links'
-          activeClassName='selected'
-          to='/building'>
-          Building Info
-        </NavLink>
-        <NavLink
-          className='nav__section-links'
-          onClick={handleLogOut}
-          to='/'>
-          Log Out
-        </NavLink>
-      </nav>
-    </div>
+  <div className="nav__section">
+    <img src={require('../../assets/profile.jpg')} alt="profile" />
+    <nav>
+      <NavLink
+        className="nav__section-links"
+        activeClassName="selected"
+        to="/profile"
+      >
+        My Profile
+      </NavLink>
+      <NavLink
+        className="nav__section-links"
+        activeClassName="selected"
+        to="/neighbors"
+      >
+        My Neighbors
+      </NavLink>
+      <NavLink
+        className="nav__section-links"
+        activeClassName="selected"
+        to="/building"
+      >
+        Building Info
+      </NavLink>
+      <NavLink className="nav__section-links" to="/account">
+        Account Settings
+      </NavLink>
+      <NavLink className="nav__section-links" onClick={handleLogOut} to="/">
+        Log Out
+      </NavLink>
+    </nav>
+  </div>
 );
 
 Nav.propTypes = {
